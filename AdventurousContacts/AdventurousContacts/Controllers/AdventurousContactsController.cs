6c4b4f7651b89ad29ddbe63deffae7e64dac2625
sc222as <sc222as@student.lnu.se>
2016-06-21 21:43:38 +0200

    Lab2FirstCommit
    Påbörjat Laboration 2

---
+++ AdventurousContacts/AdventurousContacts/Controllers/AdventurousContactsController.cs
@@ -0,0 +1,37 @@
+﻿using AdventurousContacts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdventurousContacts.Controllers
+{
+    public class AdventurousContactsController : Controller
+    {
+        private Entities _context = new Entities();
+        
+        
+        // GET: AdventurousContacts
+        public ActionResult Index()
+        {    
+            return View();
+        }
+
+
+        //POST NY CONTACT
+        [HttpPost] 
+        [ValidateAntiForgeryToken]       
+        public ActionResult Index([Bind(Include = "Firstname, Lastname, EmailAddress")]Contact model) // Behövs ContactID här?
+        {
+            if (ModelState.IsValid)
+            {
+                return View("index", model);
+            }
+            return View();
+        }
+
+
+
+    }
+}
